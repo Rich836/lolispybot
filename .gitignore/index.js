f6a09534ffcf3b50a85316e1b3a691e0b2c38349
Rich836 <41706570+Rich836@users.noreply.github.com>
2018-08-10 00:41:48 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js');
 
+
+
 const client = new Discord.Client();
+
 
 var prefix = ("-")
 
+
+
 client.login(process.env.TOKEN); //POUR LOGIN LE CLIENT
+
 
 
 const ytdl = require('ytdl-core');
